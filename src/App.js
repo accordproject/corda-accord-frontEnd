5f1b8baa45ebb6906203d3f30bd4459a29ce8e69
rogersanick <rogers.a.nick@gmail.com>
2019-02-04 13:58:26 -0500

    Committing with latest UI Updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
+const Proxy = require('braid-client').Proxy;
+
+// Connects to Braid running on the node.
+let braid = new Proxy({
+  url: "http://localhost:8080/api/"
+}, onOpen, onClose, onError, { strictSSL: false });
+
+function onOpen() { console.log('Connected to node.'); }
+function onClose() { console.log('Disconnected from node.'); }
+function onError(err) { console.error(err); }
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      nodeData: '',
+    };
+  }
+
+  startFlowButtonClicked() {
+    braid.flows.whoAmIFlow(
+      result => console.log("Hey, you're speaking to " + result + "!"),
+      err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Welcome to the Bank of Corda.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick = {() => { this.startFlowButtonClicked() }}>Start IssueCash Flow</button>
         </header>
       </div>
     );
