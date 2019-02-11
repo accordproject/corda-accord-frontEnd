a7c5bb3a34672b8f505dcb2aa795b2c07b745131
rogersanick <rogers.a.nick@gmail.com>
2019-02-11 16:06:11 -0500

    Updating with latest changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
-import axios from 'axios'
 
 const Proxy = require('braid-client').Proxy;
 
@@ -21,52 +20,30 @@ class App extends Component {
     let onError = (err) => { console.error(err)} ;
 
     this.braid = new Proxy({
-      url: 'http://localhost:9000/api/'
+      url: 'http://localhost:9002/api/'
     }, onOpen, onClose, onError, { strictSSL: false });
 
   }
-
-  addNodeButtonClickHandler() {
-      axios.get(this.state.cordaNodeWebAddress + "/api/bank/simple-cash-query").then((res) => {
-        console.log(res.data);
-        if (!this.state.cordaNodes.includes(this.state.cordaNodeWebAddress)) {
-          this.setState({
-            cordaNodes: this.state.cordaNodes.concat(this.state.cordaNodeWebAddress)
-          })
-        }
-      }).catch((e) => {
-        console.log(e);
-      })
-  }
-
-  getCashStatesButtonClickHandler() {
-    axios.get(this.state.cordaNodeWebAddress + "/api/bank/simple-cash-query").then((res) => {
-      console.log(res.data);
-    }).catch((e) => {
-      console.log(e);
+  
+  async getIssuedPromissoryNotes() {
+    console.log(this.braid);
+    let data = await this.braid.PromissoryNotesInterface.getIssuedPromissoryNotes();
+    console.log(data);
+    this.setState({
+      promissoryNotesIssues: data
     })
   }
 
-  triggerCashIssueAndPaymentFlowClickHandler() {
-    let data = {
-      "amount": 9994,
-      "issueToPartyName": "BigCorporation",
-      "notaryName": "Notary Service"
-    }
-
-    var headers = {
-      "Content-Type": "application/json" 
-    }
-
-    axios.post(this.state.cordaNodeWebAddress + "/api/bank/simple-issue-and-payment", JSON.stringify(data), headers).then((res) => {
-      console.log(res.data);
-    }).catch((e) => {
-      console.log(e);
-    })
-  }
-
-  handleNodeWebAddressChange(e) {
-    this.setState({cordaNodeWebAddress: e.target.value})
+  issuePromissoryNotes() {
+    console.log("clicked");
+    console.log(this.braid.flows.PromissoryNoteIssueFlow)
+    let braidPromise = this.braid.flows.PromissoryNoteIssueFlow("ParticipantA", "ParticipantB");
+    braidPromise.then((data) => {
+      console.log("happening");
+      console.log(data)
+    }).catch((error) => {
+      console.log(error);
+    });
   }
 
   render() {
@@ -78,17 +55,9 @@ class App extends Component {
             Welcome to the Bank of Corda.
           </p>
           <div>
-            {this.state.cordaNodes.map((cordaNode) => {
-              return (
-              <div>
-                <div>{cordaNode.toString()}</div>
-                <button onClick = {(() => this.getCashStatesButtonClickHandler())}>Query for Cash States</button>
-                <button onClick = {(() => this.triggerCashIssueAndPaymentFlowClickHandler())}>Start IssueAndPaymentFlow</button>
-              </div>)
-            })}
+            <button onClick = {(() => this.issuePromissoryNotes())}>Issue Promissory Note</button>
+            <button onClick = {(() => this.getIssuedPromissoryNotes())}>Get Promissory Notes</button>
           </div>
-          <input onChange = {(e) => { this.handleNodeWebAddressChange(e) }} placeholder = "Enter node web-server address"></input>
-          {this.state.cordaNodes.length === 0 && this.state.cordaNodeWebAddress && this.state.cordaNodeWebAddress.length > 0 ? <button onClick = {() => { this.addNodeButtonClickHandler() }}>Connect</button> : null}
         </header>
       </div>
     );
