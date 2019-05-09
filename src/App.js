ddb3cf1cc2d5f26bed3951d8c276ee1ddfeec952
Jerome Simeon <jeromesimeon@me.com>
2019-05-08 23:50:13 -0400

    (feature) Add button for flow with JSON data
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import cordalogo from './cordalogo.png';
 import aplogo from './aplogo.png';
 import './App.css';
-import { Container, Segment, Input, Form } from 'semantic-ui-react';
+import { Container, Segment } from 'semantic-ui-react';
 
 const Proxy = require('braid-client').Proxy;
 
@@ -12,7 +12,34 @@ class App extends Component {
 
     super(props);
     this.state = {
-      promissoryNotesIssued: []
+      promissoryNotesIssued: [],
+      contractText: 'THIS IS THE CONTRACT',
+      jsonData: JSON.stringify({
+        '$class': 'org.accordproject.promissorynote.PromissoryNoteContract',
+        'contractId': '157c367c-b39a-43bc-b260-07329b236ce3',
+        'amount': {
+          '$class': 'org.accordproject.money.MonetaryAmount',
+          'doubleValue': 1000,
+          'currencyCode': 'USD'
+        },
+        'date': '2018-01-30',
+        'maker': 'Daniel Selman',
+        'interestRate': 3.8,
+        'individual': true,
+        'makerAddress': '1 Main Street',
+        'lender': 'Clause',
+        'legalEntity': 'CORP',
+        'lenderAddress': '246 5th Ave, 3rd Fl, New York, NY 10001',
+        'principal': {
+          '$class': 'org.accordproject.money.MonetaryAmount',
+          'doubleValue': 500,
+          'currencyCode': 'USD'
+        },
+        'maturityDate': '2019-01-20',
+        'defaultDays': 90,
+        'insolvencyDays': 90,
+        'jurisdiction': 'New York, NY'
+      })
     };
 
     let onOpen = () => { console.log('Connected to the node.'); };
@@ -24,9 +51,10 @@ class App extends Component {
     }, onOpen, onClose, onError, { strictSSL: false });
 
     this.issuePromissoryNotes = this.issuePromissoryNotes.bind(this);
+    this.issuePromissoryNotesJSON = this.issuePromissoryNotesJSON.bind(this);
     this.getIssuedPromissoryNotes = this.getIssuedPromissoryNotes.bind(this);
   }
-  
+
   async getIssuedPromissoryNotes() {
     let data = await this.braid.PromissoryNotesInterface.getIssuedPromissoryNotes();
     this.setState({
@@ -36,6 +64,24 @@ class App extends Component {
 
   issuePromissoryNotes() {
     let braidPromise = this.braid.flows.PromissoryNoteIssueFlow();
+    this.setState({
+      loading: true
+    });
+    braidPromise.then((data) => {
+      console.log(data);
+      this.setState({
+        loading: false
+      });
+    }).catch((error) => {
+      console.log(error);
+      this.setState({
+        loading: false
+      });
+    });
+  }
+
+  issuePromissoryNotesJSON() {
+    let braidPromise = this.braid.flows.PromissoryNoteIssueJSONFlow(this.state.contractText, this.state.jsonData);
     this.setState({
       loading: true
     });
@@ -64,6 +110,7 @@ class App extends Component {
             </p>
             <div>
               {this.state.loading ? <button>Talking to the node...</button> : <button onClick = {(() => this.issuePromissoryNotes())}>Issue Promissory Note</button>}
+              {this.state.loading ? <button>Talking to the node...</button> : <button onClick = {(() => this.issuePromissoryNotesJSON())}>Issue Promissory Note from Data</button>}
               <button onClick = {(() => this.getIssuedPromissoryNotes())}>Get Promissory Notes</button>
             </div>
             <div>
